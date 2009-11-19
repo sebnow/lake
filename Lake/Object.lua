1f96bda5ca054942f201a649e157a7f4c397d8bd
Sebastian Nowicki <sebnow@gmail.com>
2009-11-19 11:53:14 +0800

    Add Object prototype
    Signed-off-by: Sebastian Nowicki <sebnow@gmail.com>

---
+++ Lake/Object.lua
@@ -0,0 +1,34 @@
+-- Copyright (c) 2009 Sebastian Nowicki <sebnow@gmail.com>
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+-- Object Root object prototype
+local Object = {}
+
+--- Clone an object.
+-- @param object the object to be cloned.
+-- @return A clone of object.
+function Object:clone(object)
+	object = object or {}
+	setmetatable(object, self)
+	self.__index = self
+	return object
+end
+
+return Object
